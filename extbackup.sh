51f2af0ffeff29ad4c90d500e01c6c8d64fa957f
highdeftant <a.allen.com@gmail.com>
2023-10-21 00:45:18 -0400

    Big update to backup script

--- extbackup.sh
+++ extbackup.sh
@@ -4,42 +4,72 @@
 
 DATE=$(date +%b-%d-%y)
 TIME=$(date +%H:%M:%S)
-SRCDIR=/home/gh0st/
-DESTUSB=/media/gh0st/mybook/os_backup/
+EXTDR="/dev/sdd1"
+CRONLOGS="$USER/Documents/cronlogs/"
+
+# Searches to see if directory is made, if not, create it
+# After directory creation, mount the drive
+function findHDD() {
+
+	if [ ! -d $BACKUPDR ]; then
+		echo "Backup Drive not located... Creating directory..."
+		mkdir -p $BACKUPDR
+	else
+		if grep -qs "$EXTDR" /proc/mounts; then
+			echo "Device is already mounted"
+		fi
+	fi
+
+	if sudo mount $EXT $BACKUPDR; then
+		echo "Backup Drive has been mounted"
+	else
+		echo "Mounting Failed! Errors sent to log file"
+		echo >>&2 $CRONLOGS/backuperr.log
+		notify-send "Mounting Failed! Errors sent to log file"
+	fi
+}
+
+#
+function backupos() {
+	sudo rm -vr .cache/mozilla/ .cache/brave*/
+	
+	if [ -d $BACKUPDR ]; then
+		notify-send "OS Backup Starting"
+		echo $DATE "@" $TIME
+		echo "Initiating Backup...."
+		sudo rsync -av --delete $HOME $BACKUPDR >> $CRONLOGS/backup.log
+		echo "Backup Completed at $TIME"
+	elif [ ! -d $BACKUPDR ]; then
+		echo "Drive not detected. Can't perform backup!"
+		echo >>&2 $CRONLOGS/backuperr.log
+	fi
+
+
+}
 
 function restoreos() {
-	DESTDR=/home/gh0st/
-	SRCDR=/media/gh0st/mybook/os_backup
-	echo "Starting Restoration..."
-	sudo rsync -av $SRCDR $DESTDR
+	if [-d $BACKUPDR]; then
+		echo "Starting Restoration..."
+		sudo rsync -av $BACKUPDR $HOME
+	fi
 }
 
-function backupos() {
-	echo $DATE "@" $TIME
-	echo "Starting Backup...."
-	sudo rm -vr .cache/mozilla/*
-	sudo rsync -av --delete $SRCDIR $DESTUSB
-	echo "Backup Completed at $TIME"
-}
 
 function updateOS() {
-	updates=('update' 'dist-upgrade' 'autoclean' 'autoremove' )
-	echo $DATE
-  echo $TIME
+	updates=('update' 'dist-upgrade' 'autoclean' 'autoremove')
+	echo $DATE "@" $TIME
+ 	echo "Starting Updater...."
 
 	for i in $updates
 	do 
 		sudo apt $i
 	done
 
- 	echo "Starting Update"
- 	echo "Update Complete"
+ 	echo "Update Completed!"
  	echo "Completed at $TIME"
 }
 
+findHDD
 backupos
-echo "\n \n \n"
-echo "-------------------------"
-echo "-------------------------"
-echo "-------------------------"
+
 exit
