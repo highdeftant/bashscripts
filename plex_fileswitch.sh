7c9dca122ae515fe18ecc12e94e8a722f6b94868
highdeftant <a.allen.com@gmail.com>
2025-06-29 21:51:51 -0400

    meow

---
+++ plex_fileswitch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This will take the file given and rsync it over to my
+# plex server for easy viewing.
+#
+# This may possibly need a delete file function
+
+get_file() {
+  file="$1"
+}
+
+
