1cee8e0fca8423de41503e2ebc702bb482f921aa
highdeftant <a.allen.com@gmail.com>
2025-07-04 23:01:56 -0400

    meow

---
+++ plex/plex_fileswitch.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# todo: Add a function to compare file size
+# with free storage on server
+
+addMovie() {
+  MOVIE=$1
+  local USER='gh0stpi'
+  local HOST='hashirama'
+  local $DEST='/home/$USER/Plex/Movies'
+
+  if [[ ! -f $MOVIE ]]; then
+    echo "[ERROR]: $MOVIE not found"
+  else
+    rsync -av -e ssh $MOVIE $USER@$HOST:$DEST
+    echo "[SUCCESS]: Moved $MOVIE to $DEST"
+  fi
+}
+
+
+# todo: Add funtion to scan for files older
+# than 30 days and prompt to delete/keep
+archiveFile() {
+  COUNT=ssh $USER@$HOST ls | wc -l
+
+}
+addMovie
