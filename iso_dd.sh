068e4506310a70ff6e8c20f79d079170170ba636
highdeftant <a.allen.com@gmail.com>
2025-07-15 22:29:36 -0400

    still not working but whatevv

--- iso_dd.sh
+++ iso_dd.sh
@@ -5,19 +5,32 @@
 # mounts your 32GB USB drive for quick ISO install swapping
 
 find_iso() {
-  ISO=0
+  NUM=0
 
   # Catches any errors if directory does not exist
   if [[ ! -d  ]]; then
     echo "[ERROR]: Directory does not exist."
   fi
 
-  for FILE in $(ls $DIR); do
-    echo "[$ISO]"
-    if [[ ${FILE##.-} == ".iso" ]]; then
+  # List all ISOs in folder by number
+  cd $1
 
+  for NUM in $(ls $DIR | wc -l); do
+    for FILE in $(ls); do
+    if [[ ${FILE##.-} == "*.iso"  ]]; then
+      echo "[$NUM]:  $ISO"
     fi
   done
+done
+
+}
 
 
+checkUUID() {
+  UUID=""
+  lsusb
+
+  if [[ "$DEV" == "$UUID" ]]; then
+    find_iso
+  fi
 }
