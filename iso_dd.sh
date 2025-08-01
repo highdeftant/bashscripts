32d6c5eae0c26111e749f53aca324c53cad8ee25
highdeftant <a.allen.com@gmail.com>
2025-08-01 13:51:49 -0400

    apparently my comments look like AI but i'm just forgetful

--- iso_dd.sh
+++ iso_dd.sh
@@ -4,8 +4,21 @@
 # Lists all ISO's found in numerical selection then
 # mounts your 32GB USB drive for quick ISO install swapping
 
+# Checks UUID for specific UUID if the same drive is always used
+checkUUID() {
+  DEV=$(blkid | awk '/UUID/ {print $3}')
+  UUID2="65d75412-118b-49d6-b2ce-2bf1448c711b"
+
+  for DEVICE in $DEV; do
+    if [[ "$DEV" == "$UUID" ]]; then
+      findIso $1
+    else
+      echo "[ERROR]: UUID Not Found. USB may not be mounted."
+    fi
+  done
+}
+
 findIso() {
-  # Checks directory existence
   if [[ ! -d $1 ]]; then
     echo "[ERROR]: Directory does not exist."
     exit
@@ -23,11 +36,11 @@ findIso() {
     fi
   done
 
-  echo "**** ISO BOOTER V0.1****"
+  echo "----- ISO BOOTER V0.1 -----"
   for star in {0..4}; do
-    echo "*                   *"
+    echo "|                   |"
   done
-  echo "----- CHOOSE ISO -----"
+  echo -e "----- CHOOSE ISO -----\n"
 
   for NUM in ${!ISOLIST[@]}; do
     for FILE in ${ISOLIST[@]}; do
@@ -36,25 +49,9 @@ findIso() {
    done
  done
 
-  # Ask user for ISO Choice
   read -r "IMG/ISO to Mount" CHOICE
 }
 
-# Checks UUID for specific UUID if the same drive is always used
-checkUUID() {
-  DEV=$(blkid | awk '/UUID/ {print $3}')
-  UUID2="65d75412-118b-49d6-b2ce-2bf1448c711b"
-
-  for DEVICE in $DEV; do
-    if [[ "$DEV" == "$UUID" ]]; then
-      findIso $1
-    else
-      echo "[ERROR]: UUID Not Found. USB may not be mounted."
-    fi
-  done
-}
-
-# Takes the selected ISO and burns it to the disk
 burnISO() {
   DEST=$1
   DEV=$2
@@ -64,3 +61,4 @@ burnISO() {
 
 checkUUID
 findIso
+burnISO
