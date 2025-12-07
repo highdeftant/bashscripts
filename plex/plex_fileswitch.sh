bd9ce80f2f4c7f39ff0446bb391718c8c7994beb
highdeftant <a.allen.com@gmail.com>
2025-12-07 14:43:53 -0500

    fixes

--- plex/plex_fileswitch.sh
+++ plex/plex_fileswitch.sh
@@ -3,7 +3,7 @@
 # todo: Add a function to compare file size
 # with free storage on server
 
-DEST="Plex/Media/Movies"
+DEST="Plex/Media/"
 
 addMovie() {
   MOVIE="$1"
@@ -12,7 +12,7 @@ addMovie() {
   local HOST="hashirama"
 
   if [[ ! -f "$MOVIE" ]]; then
-    echo "[ERROR]: $MOVIE not found"
+    echo "[ERROR]: "$MOVIE" not found"
   else
     rsync -av -e ssh -P "$MOVIE" "$USER"@"$HOST":"$DEST"
     echo "[SUCCESS]: Moved "$MOVIE" to "$DEST""
@@ -25,27 +25,27 @@ addMovie() {
 # todo: Add funtion to scan for files older
 # than 30+ days and prompt to delete/keep
 
-archiveFile() {
-  LISTFILE=$(ssh $USER@$HOST ls -A "$DEST")
-
-  # Iterates through folder and checks
-  # last modded date of file
-
-
-  #this function needs some rearrangement
-  for FILE in $LISTFILE; do
-    [[ -f $FILE ]]; || continue
-    EXT=${FILE##*.-}
-    ARCHIVE=`date -d "30 days ago" +%s`
-    LASTMOD=`date -c "%Y" "$FILE"`
-    declare -A OLDMOVIES=()
-    case $EXT in
-      mp4|MP4)
-        OLDMOVIES["Archive"]="$FILE"
-        OLDMOVIES["Times"]=$LASTMOD
-        ;;
-    esac
-  done
-}
-
-addMovie $1
+#archiveFile() {
+#  LISTFILE=$(ssh $USER@$HOST ls -A "$DEST")
+#
+#  # Iterates through folder and checks
+#  # last modded date of file
+#
+#
+#  #this function needs some rearrangement
+#  for FILE in $LISTFILE; do
+#    [[ -f $FILE ]]; || continue
+#    EXT=${FILE##*.-}
+#    ARCHIVE=`date -d "30 days ago" +%s`
+#    LASTMOD=`date -c "%Y" "$FILE"`
+#    declare -A OLDMOVIES=()
+#    case $EXT in
+#      mp4|MP4)
+#        OLDMOVIES["Archive"]="$FILE"
+#        OLDMOVIES["Times"]=$LASTMOD
+#        ;;
+#    esac
+#  done
+#}
+#
+addMovie "$1"
